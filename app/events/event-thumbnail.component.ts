a111da38fd5cd64fbcdc567053ec0af921ee151e
Grzegorz Swierczek <g.swierczek@gmail.com>
2017-10-28 20:49:19 +0200

    Event and Toastr services added

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import {Component, Input} from '@angular/core';
         <div class="well hoverwell thumbnail">
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
-            <div [ngSwitch]="event?.time">
+            <div [ngStyle]="getStartTimeStyle()"[ngSwitch]="event?.time">
                 Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -19,7 +19,7 @@ import {Component, Input} from '@angular/core';
             </div>
         </div>
     `,
-    styles: [`
+    styles: [`        
         .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb;}
@@ -27,4 +27,11 @@ import {Component, Input} from '@angular/core';
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'}
+        return {}
+    }
+
 }
